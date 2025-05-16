9203f118c3dc3881c63e9c243869391b3d57d9b4
Kiet Tran <charlieismylife2003@gmail.com>
2025-05-16 21:32:32 +0700

    fix bug

--- src/app/(user)/(root)/danh-gia-cv/page.jsx
+++ src/app/(user)/(root)/danh-gia-cv/page.jsx
@@ -1,16 +1,14 @@
-import Spinner from '@/components/views/Spinner';
 import CVPreviewer from '@/components/views/user/danh-gia-cv/CVPreviewer';
 import CVReview from '@/components/views/user/danh-gia-cv/CVReview';
 import CVUpload from '@/components/views/user/danh-gia-cv/CVUpload';
 import MainLayout from '@/layout/MainLayout';
-import {Suspense} from 'react';
+
+export const dynamic = 'force-dynamic';
 
 const page = () => {
   return (
     <MainLayout className="mt-8 space-y-8">
-      <Suspense fallback={<Spinner isCentered />}>
-        <CVUpload />
-      </Suspense>
+      <CVUpload />
       <CVReview />
       <CVPreviewer />
     </MainLayout>
